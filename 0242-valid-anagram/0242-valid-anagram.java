d16a28fc442e6681ad7e457e4780b103344fd014
vivek005MB <79520303+vivek005MB@users.noreply.github.com>
2023-05-31 03:31:30 +0530

    Time: 2 ms (99.75%), Space: 43.6 MB (16.93%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,9 +1,31 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        return sortingMethod(s, t); // o(nlogn)     O(n)
+        // return sortingMethod(s, t); // o(nlogn)     O(n)
         // return hashmapMethod(s, t);
+        return countarray(s,t);
         
         
+    }
+    public boolean countarray(String s, String t)
+    {
+        int[] arr1 = new int[26];
+        for(char ch:s.toCharArray())
+        {
+            arr1[ch-'a']++;
+        }
+        int[] arr2 = new int[26];
+        for(char ch:t.toCharArray())
+        {
+            arr2[ch-'a']++;
+        }
+        for(int i = 0; i <26; i++)
+        {
+            if(arr1[i] != arr2[i])
+            {
+                return false;
+            }
+        }
+        return true;
     }
     public boolean hashmapMethod(String s, String t)
     {
